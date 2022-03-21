32ffe04d7944dc0cdbb10c661ac90aa4bcb3eb72
ParkHaemi <qkrgoal18@gmail.com>
2022-03-21 15:44:04 +0900

    Modify Tetris.java, create scece and start the game

--- src/main/java/com/example/tetris/Tetris.java
+++ src/main/java/com/example/tetris/Tetris.java
@@ -1,6 +1,10 @@
 package com.example.tetris;
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
@@ -25,12 +29,105 @@ public class Tetris extends Application {
     private static Form object;
     private static Scene scene = new Scene(groupe, XMAX + 150, YMAX);
     public static int score = 0;
+    public static int top = 0;
     private static boolean game = true;
     private static Form nextObj = Controller.makeRect();
-    private static int LinesNo = 0;
+    private static int linesNo = 0;
 
+    // creating scene and start the game
+    public void main(String[] args) { launch(args); }
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
+        for (int[] a: MESH) {
+            Arrays.fill(a, 0);
+        }
 
+        // Creating score and level text
+        Line line = new Line(XMAX, 0, XMAX, YMAX);
+        Text scoretext = new Text("Score: ");
+        scoretext.setStyle("-fx-font: 20 arial;");
+        scoretext.setY(50);
+        scoretext.setX(XMAX + 5);
+        Text level = new Text("Lines: ");
+        level.setStyle("-fx-font: 20 arial;");
+        level.setY(100);
+        level.setX(XMAX + 5);
+        level.setFill(Color.GREEN);
+        groupe.getChildren().addAll(scoretext, line, level);
+
+        // Creating first block and the stage
+        Form a = nextObj;
+        groupe.getChildren().addAll(a.a, a.b, a.c, a.d);
+        moveOnKeyPress(a);
+        object = a;
+        nextObj = Controller.makeRect();
+        stage.setScene(scene);
+        stage.setTitle("T E T R I S");
+        stage.show();
+
+        // Timer
+        Timer fall = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (object.a.getY() == 0 || object.b.getY() == 0 || object.c.getY() == 0 || object.d.getY() == 0)
+                            top++;
+                        else
+                            top = 0;
+
+                        if (top == 2) {
+                            // Game OVER
+                            Text over = new Text("GAME OVER");
+                            over.setFill(Color.RED);
+                            over.setStyle("-fx-font: 70 arial;");
+                            over.setY(250);
+                            over.setX(10);
+                            groupe.getChildren().add(over);
+                            game = false;
+                        }
+
+                        // Exit
+                        if (top == 15) {
+                            System.exit(0);
+                        }
+
+                        if (game) {
+                            MoveDown(object);
+                            scoretext.setText("Score: " + Integer.toString(score));
+                            level.setText("Lines: " + Integer.toString(linesNo));
+                        }
+                    }
+                });
+            }
+        };
+        fall.schedule(task, 0, 300);
     }
+
+    private  void moveOnKeyPress(Form form) {
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case RIGHT:
+                        Controller.MoveRight(form);
+                        break;
+                    case DOWN:
+                        MoveDown(form);
+                        score++;
+                        break;
+                    case LEFT:
+                        Controller.MoveLeft(form);
+                        break;
+                    case UP:
+                        MoveTurn(form);
+                        break;
+                }
+            }
+        });
+    }
+
+
 }
